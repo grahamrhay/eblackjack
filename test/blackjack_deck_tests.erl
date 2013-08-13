60068ce23a2d1c66c15f8290af6ded1b652fa280
Graham Hay <grahamrhay@gmail.com>
2013-08-13 13:54:00 +0100

    Use macro in tests

--- test/blackjack_deck_tests.erl
+++ test/blackjack_deck_tests.erl
@@ -4,6 +4,8 @@
 
 -include_lib("../src/blackjack.hrl").
 
+-define(CARD(Card), #blackjack_card{ card = Card }).
+
 new_test() ->
     Deck = blackjack_deck:new(),
     ?assertEqual(52, length(Deck)),
@@ -13,23 +15,23 @@ new_test() ->
     ?assertEqual(13, length(lists:filter(fun(C) -> C#blackjack_card.suit =:= diamonds end, Deck))).
 
 possible_scores_test() ->
-    ?assertEqual([1, 11], blackjack_deck:possible_scores([#blackjack_card { card = ace }])),
-    ?assertEqual([9], blackjack_deck:possible_scores([#blackjack_card { card = 9 }])),
-    ?assertEqual([10], blackjack_deck:possible_scores([#blackjack_card { card = jack }])),
-    ?assertEqual([10], blackjack_deck:possible_scores([#blackjack_card { card = queen }])),
-    ?assertEqual([10], blackjack_deck:possible_scores([#blackjack_card { card = king }])),
-    ?assertEqual([2, 12, 22], blackjack_deck:possible_scores([#blackjack_card { card = ace }, #blackjack_card { card = ace }])),
-    ?assertEqual([19, 29], blackjack_deck:possible_scores([#blackjack_card { card = ace }, #blackjack_card { card = 8 }, #blackjack_card { card = king }])).
+    ?assertEqual([1, 11], blackjack_deck:possible_scores([?CARD(ace)])),
+    ?assertEqual([9], blackjack_deck:possible_scores([?CARD(9)])),
+    ?assertEqual([10], blackjack_deck:possible_scores([?CARD(jack)])),
+    ?assertEqual([10], blackjack_deck:possible_scores([?CARD(queen)])),
+    ?assertEqual([10], blackjack_deck:possible_scores([?CARD(king)])),
+    ?assertEqual([2, 12, 22], blackjack_deck:possible_scores([?CARD(ace), ?CARD(ace)])),
+    ?assertEqual([19, 29], blackjack_deck:possible_scores([?CARD(ace), ?CARD(8), ?CARD(king)])).
 
 hit_test() ->
-    Card = #blackjack_card { card = ace, suit = spades },
+    Card = ?CARD(ace),
     Deck = [Card],
     {NewDeck,NewCards} = blackjack_deck:hit(Deck, []),
     ?assertEqual([], NewDeck),
     ?assertEqual([Card], NewCards).
 
 bust_test() ->
-    ?assertEqual(false, blackjack_deck:bust([#blackjack_card { card = ace }])), % 1
-    ?assertEqual(false, blackjack_deck:bust([#blackjack_card { card = king }, #blackjack_card { card = king }])), % 20
-    ?assertEqual(false, blackjack_deck:bust([#blackjack_card { card = ace }, #blackjack_card { card = king }, #blackjack_card { card = 3 }])), % 14, 24
-    ?assertEqual(true, blackjack_deck:bust([#blackjack_card { card = jack }, #blackjack_card { card = jack }, #blackjack_card { card = 2 }])). % 22
+    ?assertEqual(false, blackjack_deck:bust([?CARD(ace)])), % 1
+    ?assertEqual(false, blackjack_deck:bust([?CARD(king), ?CARD(king)])), % 20
+    ?assertEqual(false, blackjack_deck:bust([?CARD(ace), ?CARD(king), ?CARD(3)])), % 14, 24
+    ?assertEqual(true, blackjack_deck:bust([?CARD(jack), ?CARD(jack), ?CARD(2)])). % 22
